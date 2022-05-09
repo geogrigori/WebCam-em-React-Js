22eb5e790b5cc9346bc5d5da2063c12ddc9e4bf4
Geovana Grigorio <geovanagomes832@gmail.com>
2022-05-09 17:03:22 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer style={{backgroundColor:'black',color:'white',padding:'10px 0'}}>
-            Made by Sristi Chowdhury
+            Made by Geovana Grigorio
         </footer>
     )
 }
